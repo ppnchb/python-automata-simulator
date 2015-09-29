ae04e225de169a1038fb7b423dde08bf7404f173
Hyunsoo <ppnchb@outlook.com>
2015-09-29 21:49:42 +0900

    update test package

--- test/main.py
+++ test/main.py
@@ -6,6 +6,11 @@ from automata.automata import MealyMachine
 
 if __name__ == "__main__":
     machine = MealyMachine('mealy.csv')
-    string = input(">> ")
-    output = machine(string)
+    output = []
+    symbol = input(">> ")
+    while symbol is not "":
+        outputSymbol = machine(symbol)
+        output.append(outputSymbol)
+        print(outputSymbol)
+        symbol = input(">> ")
     print(output)
